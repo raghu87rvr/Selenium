e9747e478f7ecda16615081d90bea6e8fd4d55ac
Mahesh S <smah@SMAH8.auth.hpicorp.net>
2017-11-06 20:21:42 +0530

    test Push

--- Selenium/src/main/java/basepage/BasePage.java
+++ Selenium/src/main/java/basepage/BasePage.java
@@ -5,6 +5,7 @@ public class BasePage {
 	public static void main(String[] args) {
 		
 		System.out.println("program is executing");
+		System.out.println("executing from remote works :)");
 		
 	}
 
